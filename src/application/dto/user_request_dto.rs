faa16f630e757062c0a55a1224aba22f7c6a50f2
kazuma0606 <yoshimura.hisa@gmail.com>
2025-07-08 22:37:25 +0900

    ユーザーDto追加

--- src/application/dto/user_request_dto.rs
+++ src/application/dto/user_request_dto.rs
@@ -1,3 +1,27 @@
 //application/dto/user_request_dto.rs
 // HTTP Request DTO
 // 2025/7/8
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CreateUserRequestDto {
+    pub email: String,
+    pub name: String,
+    pub password: String,
+    pub phone: Option<String>,
+    pub birth_date: Option<String>,
+}
+
+impl CreateUserRequestDto {
+    /// バリューオブジェクト変換＋バリデーション
+    pub fn to_value_objects(&self) -> (String, String, String, Option<String>, Option<String>) {
+        (
+            self.email.clone(),
+            self.name.clone(),
+            self.password.clone(),
+            self.phone.clone(),
+            self.birth_date.clone(),
+        )
+    }
+}
--- src/application/dto/user_response_dto.rs
+++ src/application/dto/user_response_dto.rs
@@ -1,3 +1,14 @@
 //application/dto/user_response_dto.rs
 // HTTP Response DTO
 // 2025/7/8
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct UserResponseDto {
+    pub id: String,
+    pub email: String,
+    pub name: String,
+    pub phone: Option<String>,
+    pub birth_date: Option<String>,
+}
